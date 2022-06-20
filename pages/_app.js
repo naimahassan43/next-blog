7b9dc2bded7ff85459b845ee70560f8dc3f0098d
Naima Hassan <naima.hassan434@gmail.com>
2022-06-20 14:26:07 +0100

    Styling-Global, Styled JSX and Images

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "../styles/globals.css";
 import NavBar from "./../components/NavBar";
+import { Head } from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <header>
         <NavBar />
       </header>
