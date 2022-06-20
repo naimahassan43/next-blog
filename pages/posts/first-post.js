ad45f6be22b09189a7bd8b0cb3c8d2bb9b9256ae
Naima Hassan <naima.hassan434@gmail.com>
2022-06-20 22:18:51 +0100

    Lib Module

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,9 @@
-import { readFile } from "fs/promises";
 import Head from "next/head";
+import { getPost } from "../../lib/posts";
 
 export async function getStaticProps() {
   console.log("first page getStaticProps()");
-  const data = await readFile("contents/posts/first-post.json", "utf8");
-  const post = JSON.parse(data);
+  const post = await getPost("first-post");
   return {
     props: { post },
   };
