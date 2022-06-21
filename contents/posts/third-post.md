e32afc8c66220498a942e37eaac82953d7adac11
Naima Hassan <naima.hassan434@gmail.com>
2022-06-21 11:35:08 +0100

    Listing Files in a Folder

---
+++ contents/posts/third-post.md
@@ -0,0 +1,15 @@
+---
+date: "20/06/2022"
+title: "Third Post"
+---
+
+# Third Post from Marked
+
+This is my Third post
+
+Here's some **bold text**.
+
+And a list:
+
+- One
+- Two
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,15 +1,16 @@
 import Head from "next/head";
-import { getPost } from "../../lib/posts";
+import { getPost, getSlugs } from "../../lib/posts";
 
-export const getStaticPaths = async () => {
+export async function getStaticPaths() {
+  const slugs = await getSlugs();
   return {
-    paths: [
-      { params: { slug: "first-post" } },
-      { params: { slug: "second-post" } },
-    ],
+    paths: slugs.map((slug) => ({
+      params: { slug },
+    })),
+
     fallback: false,
   };
-};
+}
 
 export async function getStaticProps({ params: { slug } }) {
   console.log("post page getStaticProps()", slug);
