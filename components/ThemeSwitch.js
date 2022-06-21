72cd084fac22364f5dc99743830039d917e6fef8
Naima Hassan <naima.hassan434@gmail.com>
2022-06-21 17:48:39 +0100

    ThemeSwitch Component

---
+++ components/ThemeSwitch.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function ThemeSwitch() {
+  const [darkMode, setDarkMode] = useState(false);
+  console.log("dark mode themeSwitch", darkMode);
+
+  const text = darkMode ? "Light Mode" : "Dark Mode";
+  return (
+    <>
+      <button onClick={() => setDarkMode(!darkMode)}>{text}</button>;
+      <style jsx>
+        {`
+          button {
+            background: none;
+            border: none;
+            cursor: pointer;
+          }
+        `}
+      </style>
+    </>
+  );
+}
